2e647c367ec2cee0ec956292bc0f54990613e373
Jason Lai <jason99.lai@gmail.com>
2019-01-18 14:50:14 +0800

    Update demo.cpp

--- demo.cpp
+++ demo.cpp
@@ -38,18 +38,18 @@ int main()
     cout << endl;
 
     // Testing User's input data
-//    BigInteger dividend, divisor;
-//    cout << "Testing User's input data..." << endl;
-//    cout << "Enter a dividend integer number: "; cin >> dividend;
-//    cout << "Enter a divisor integer number: "; cin >> divisor;
-//    quotient = dividend / divisor;
-//    remainder = dividend % divisor;
-//    cout  << "quotient = " << quotient << endl;
-//    cout  << "remainder = " << remainder << endl;
-//    if (dividend == (quotient * divisor + remainder))
-//        cout << "The dividend is consistent with quotient * divisor + remainder" << endl;
-//    else
-//        cout << "The dividend is not consistent with quotient * divisor + remainder" << endl;
+    BigInteger dividend, divisor;
+    cout << "Testing User's input data..." << endl;
+    cout << "Enter a dividend integer number: "; cin >> dividend;
+    cout << "Enter a divisor integer number: "; cin >> divisor;
+    quotient = dividend / divisor;
+    remainder = dividend % divisor;
+    cout  << "quotient = " << quotient << endl;
+    cout  << "remainder = " << remainder << endl;
+    if (dividend == (quotient * divisor + remainder))
+        cout << "The dividend is consistent with quotient * divisor + remainder" << endl;
+    else
+        cout << "The dividend is not consistent with quotient * divisor + remainder" << endl;
 
     system("pause");
 
