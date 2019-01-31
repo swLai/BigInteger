808b80d5f3bee5d68cdef449c399dfb299a5f71a
Jason Lai <jason99.lai@gmail.com>
2019-01-31 16:49:59 +0800

    Update BigInteger.cpp

--- src/BigInteger.cpp
+++ src/BigInteger.cpp
@@ -317,10 +317,8 @@ static BigInteger subtract(const BigInteger &lhs, const BigInteger &rhs)
 static BigInteger multiply_karatsuba(const BigInteger &lhs, const BigInteger &rhs)
 {
     BigInteger zero = ZERO;
-    BigInteger a0, a1, b0, b1;
-    BigInteger r, p, q;
 
-   unsigned n = max(lhs.get_digits(), rhs.get_digits()), m;
+   unsigned n = max(lhs.get_digits(), rhs.get_digits());
 
     if (lhs == zero || rhs == zero)
         return zero;
@@ -332,10 +330,12 @@ static BigInteger multiply_karatsuba(const BigInteger &lhs, const BigInteger &rh
     }
     else
     {
-        m  = floor(n / 2);
-        a1 = shift_10r2p(lhs, -m); a0 = rem_10r2p(lhs, m);
-        b1 = shift_10r2p(rhs, -m);
-        b0 = rem_10r2p(rhs, m);
+        BigInteger lhs_abs(abs(lhs)), rhs_abs(abs(rhs));
+        BigInteger a0, a1, b0, b1;
+        BigInteger r, p, q;
+        unsigned m  = floor(n / 2);
+        a1 = shift_10r2p(lhs_abs, -m); a0 = rem_10r2p(lhs_abs, m);
+        b1 = shift_10r2p(rhs_abs, -m); b0 = rem_10r2p(rhs_abs, m);
         r = multiply_karatsuba(a1 + a0, b1 + b0);
         p = multiply_karatsuba(a1, b1);
         q = multiply_karatsuba(a0, b0);
@@ -451,8 +451,9 @@ static BigInteger divide_iteration(const BigInteger &dividend, const BigInteger 
     unsigned divisor_leadings_len = divisor_words_len > 2 ? 3 : divisor_words_len > 1 ? 2 : 1;
 #endif // MULTIPLY
 
-    BigInteger rem_k(dividend), quo_k;
-    while (rem_k >= divisor)
+    BigInteger divisor_abs = abs(divisor);
+    BigInteger rem_k(abs(dividend)), quo_k;
+    while (rem_k >= divisor_abs)
     {
         unsigned rem_k_words_len = rem_k.get_words_len();
 #if MULTIPLY == KARATSUBA
